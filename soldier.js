6bab44241b91965c635572d4a21ce3ccccae45b1
WetFlamer <xamidchabaev04@gmail.com>
2022-11-01 13:25:22 +0300

    Добавлены объекты

---
+++ soldier.js
@@ -0,0 +1,20 @@
+const object = {
+    name: 'John',
+    health: 10,
+    weapon: {
+        weapon_name: 'AK47',
+        ammo: 30
+    },
+    supplies: 3,
+
+    shoot: function () {
+
+    },
+    reload: function () {
+
+    },
+    attack: function {
+
+    }
+    
+}
