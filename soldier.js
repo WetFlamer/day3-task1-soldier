56ef2b531fde66f3813876c6ca2fc05f3f56524f
WetFlamer <xamidchabaev04@gmail.com>
2022-11-01 14:05:35 +0300

    Добавлены методы: Shoot, Reload, Hurt

--- soldier.js
+++ soldier.js
@@ -1,20 +1,37 @@
 const object = {
-    name: 'John',
-    health: 10,
-    weapon: {
-        weapon_name: 'AK47',
-        ammo: 30
-    },
-    supplies: 3,
+  name: "John",
+  health: 10,
+  weapon: {
+    weapon_name: "AK47",
+    ammo: 0,
+  },
+  supplies: 3,
 
-    shoot: function () {
+  shoot: function () {
+    if (this.weapon.ammo > 0) {
+      this.weapon.ammo--;
+      console.log("бах-бах");
+    } else console.log("обойма пуста. Перезарядитесь");
+  },
 
-    },
-    reload: function () {
-
-    },
-    attack: function {
-
+  reload: function () {
+    if (this.weapon.ammo === 0 && this.supplies > 0) {
+      this.weapon.ammo = 1;
+      this.supplies--;
+      console.log("Перезарядка...");
     }
-    
-}
+    if (this.supplies <= 0) {
+      console.log("Не осталось припасов");
+    }
+  },
+  hurt: function () {
+    this.health--
+    if (this.health < 10){
+        console.log(`Осталось ${this.health} единиц здоровья!`)
+    }
+    if(this.health === 0) {
+console.log("Ты проиграл!")
+    }
+    return
+  },
+};
